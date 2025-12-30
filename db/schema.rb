533b9391b40d300f556c93181c80e55e11268866
Chris Lucas <chris@cjl.io>
2025-12-30 11:01:00 -0500

    Add 2026 dates (#18)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_10_165426) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_30_154225) do
   create_table "attendance_windows", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.date "end_date"
